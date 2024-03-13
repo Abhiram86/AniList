cedb93ca2b1245940f1253889503e3eb71b418d6
Abhiram86 <abhiramalla86@gmail.com>
2024-03-13 20:07:43 +0530

    vercel.json updated

--- AniSearch/server/server.js
+++ AniSearch/server/server.js
@@ -9,14 +9,14 @@ import saveRouter from './routes/save.js';
 
 import dotenv from 'dotenv';
 
-app.use(cors());
-
 dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 const dburl = "mongodb+srv://Abhiram:8186087139@cluster0.r8gp8sq.mongodb.net/AniList?retryWrites=true&w=majority";
+
+app.use(cors());
 
 const route = express.Router();
 
