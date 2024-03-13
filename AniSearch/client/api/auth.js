818db5e902967feaca21a4ef05752fd0d49d97be
Abhiram86 <abhiramalla86@gmail.com>
2024-03-13 18:55:16 +0530

    frontend api routes updated

--- AniSearch/client/api/auth.js
+++ AniSearch/client/api/auth.js
@@ -1,13 +1,13 @@
 import Axios from "axios";
 
 export const signup = async (userData) => {
-    const user = await Axios.post('http://localhost:3001/auth/register', userData);
+    const user = await Axios.post('https://ani-list-one.vercel.app/auth/register', userData);
     console.log(user.data);
     return user.data;
 }
 
 export const login = async (userData) => {
-    const user = await Axios.post('http://localhost:3001/auth/login', userData);
+    const user = await Axios.post('https://ani-list-one.vercel.app/auth/login', userData);
     localStorage.setItem('user', JSON.stringify(user.data))
     return user.data;
 }
--- AniSearch/client/api/save.js
+++ AniSearch/client/api/save.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const getSavedData = async (email) => {
-    const savedData = await axios.post("http://localhost:3001/save/saved", {email})
+    const savedData = await axios.post("https://ani-list-one.vercel.app/save/saved", {email})
     return savedData.data
 }
 export const saveData = async (showObj) => {
-    const response = await axios.put("http://localhost:3001/save/save", showObj)
+    const response = await axios.put("https://ani-list-one.vercel.app/save/save", showObj)
     console.log(response.data.data);
     return response.data.data;
 }
 export const removeData = async (id, email) => {
-    await axios.put("http://localhost:3001/save/remove", {id, email})
+    await axios.put("https://ani-list-one.vercel.app/save/remove", {id, email})
 }
